36396da00c5e358ad25ee41d72a552e086faac05
wubu <wubeabera123@gmail.com>
2023-08-25 14:19:01 +0300

    navbar for large, medium and small screen device

--- src/components/Frontinfo.jsx
+++ src/components/Frontinfo.jsx
@@ -1,15 +1,17 @@
-import male from "../Images/male.jfif";
+import male from "../Images/male.jpg";
 
 const Frontinfo = () => {
     return (  
       <div className="row mt-5">
-         <div className="col" style={{marginLeft: "3rem",marginTop: "6rem"}}>
-            <h1 style={{color: "#333333",fontWeight: "650",fontSize: "3rem"}}>We Build Web Project that Drives Sales.</h1>
-            <p style={{color: "#555555"}}>Product development from the scratch, gathering a dedicated team, or staff augumentation -- we can pick it up right where you left off.</p>
-            <button className="btn" style={{marginRight: "1rem",backgroundColor: "#6E368C",color: "white",width: "8rem",height: "2.4rem",fontSize: "0.8rem"}}>Get In Touch</button>
-            <button className="btn" style={{backgroundColor: "white",color: "#222222",width: "8rem",height: "2.4rem",fontSize: "0.8rem",borderColor: "black", fontWeight: "500", textShadow: "6 4 8px rgba(0.4, 0.6, 0.2, 0.2)"}}>Check Our Cases</button>
+         <div className="col" style={{marginLeft: "5rem",marginTop: "6rem"}}>
+            <h1 style={{color: "#201f2f",fontWeight: "650",fontSize: "3.5rem",whiteSpace: "nowrap"}}>We Build Web Projects <br/>that Drives Sales.</h1>
+            <p style={{color: "#201f2f",fontWeight: "500",fontSize: "1.2rem",marginTop: "1rem",lineHeight: "2rem"}}>Product development from the scratch, gathering a <br/> dedicated team, or staff augumentation -- we can <br/>pick it up right where you left off.</p>
+            <div className="row">
+              <button className=" btn" style={{marginRight: "1rem",backgroundColor: "#6E368C",color: "white",width: "8rem",height: "2.4rem",fontSize: "0.8rem"}}>Get In Touch</button>
+              <button className="btn" style={{backgroundColor: "white",color: "#222222",width: "8rem",height: "2.4rem",fontSize: "0.8rem",borderColor: "black", fontWeight: "500", textShadow: "6 4 8px rgba(0.4, 0.6, 0.2, 0.2)"}}>Check Our Cases</button>
+            </div>
          </div>
-         <div className="col" style={{marginLeft: "10rem"}}>
+         <div className="col" style={{marginLeft: "12rem"}}>
            <img src={male} style={{width: "20rem"}} />
          </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,46 @@
 import logo from "../Images/logo.png"
+import { useState } from "react";
 
 const Navbar = () => {
-    return (  
-        <div className="naba d-flex" style={{alignItems: "center"}}>
-            <div className="logo d-flex mt-3" style={{marginLeft: "3rem"}}>
-                <img src={logo} className="onionlogo" style={{width:"1.8rem",height: "1.8rem",marginTop: "0.3rem",marginRight: "0.5rem"}} alt=""/>
-                <p style={{fontWeight: "bold",fontSize: "1.5rem"}}>OnionStudio</p>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+    return ( 
+      <header>
+         <nav className="container navbar navbar-expand-sm  navbar-light">
+            <div className="logo d-flex">
+              <img src={logo} className="onionlogo" style={{width:"1.8rem",height: "1.8rem",marginTop: "0.3rem",marginRight: "0.5rem"}}/>
+              <p style={{fontWeight: "bold",fontSize: "1.5rem",color: "#201f2f"}}>OnionStudio</p> 
             </div>
-            <div>
-               <nav className="navbar navbar-expand-lg" style={{marginLeft: "8rem"}}>
-                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                   <span className="navbar-toggler-icon"></span>
-                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav">
-                    
-                     <li className="nav-item active" style={{marginRight: "1.4rem"}}>
-                       <a className="nav-link" href="#" style={{color:"#444444",fontWeight: "bold"}}>About</a>
-                     </li>
-                     <li className="nav-item" style={{marginRight: "1.4rem"}}>
-                       <a className="nav-link" href="#" style={{color:"#444444",fontWeight: "bold"}}>Services</a>
-                     </li>
-                     <li className="nav-item" style={{marginRight: "1.4rem"}}>
-                       <a className="nav-link" href="#" style={{color:"#444444",fontWeight: "bold"}}>Case Studies</a>
-                     </li>
-                     <li className="nav-item" style={{marginRight: "1.4rem"}}>
-                       <a className="nav-link" href="#" style={{color:"#444444",fontWeight: "bold"}}>Careers</a>
-                     </li>
-                     <li className="nav-item">
-                       <a className="nav-link" href="#" style={{color:"#444444",fontWeight: "bold"}}>Contact</a>
-                     </li>
-                   </ul>
-                 </div>
-             </nav>
-            </div> 
-          <button className="btn ms-auto" style={{marginRight: "4rem",backgroundColor: "#6E368C",color: "white",width: "8rem",height: "2.4rem",fontSize: "0.8rem"}}>Get In Touch</button>
-        </div>
+           <div className="ms-auto"> 
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+           </div>
+           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav">
+                 <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                 </li>
+                 <li className="nav-item">
+                    <a className="nav-link" href="#">Services</a>
+                 </li>
+                 <li className="nav-item">
+                    <a className="nav-link" href="#">Case Studies</a>
+                 </li>
+                 <li className="nav-item">
+                    <a className="nav-link" href="#">Careeres</a>
+                 </li>
+                 <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                 </li>
+               </ul>
+            </div>
+            <button className="btn" style={{backgroundColor: "#6E368C",color: "white",width: "8.5rem",height: "2.4rem",fontSize: "0.8rem",marginRight: "4rem"}}>Get In Touch</button>
+         </nav>
+     </header>  
     );
 }
  
